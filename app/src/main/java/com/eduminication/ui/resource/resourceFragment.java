16bbb6ed6036f3e631ebc82893d1d4354d6e486e
htliang25 <648029539@qq.com>
2019-11-27 01:28:48 +0800

    change

--- app/src/main/java/com/eduminication/ui/resource/resourceFragment.java
+++ app/src/main/java/com/eduminication/ui/resource/resourceFragment.java
@@ -8,17 +8,29 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import com.eduminication.R;
 
 public class resourceFragment extends Fragment {
 
     private resourceViewModel resourceViewModel;
+    private RecyclerView mRecyclerView;
+    private LinearLayoutManager mLayoutManager;
+    private RecyclerView.Adapter mAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
         resourceViewModel =
                 ViewModelProviders.of(this).get(resourceViewModel.class);
         View root = inflater.inflate(R.layout.fragment_resource, container, false);
+        mRecyclerView = root.findViewById(R.id.resource_view);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+
         return root;
     }
 }
