35d0a45d0bf5b27b22cd7ecb06d654d0d2475f73
htliang25 <648029539@qq.com>
2019-11-26 22:39:05 +0800

    change

--- app/src/main/java/com/eduminication/ui/tools/ToolsFragment.java
+++ app/src/main/java/com/eduminication/ui/tools/ToolsFragment.java
@@ -20,7 +20,7 @@ public class ToolsFragment extends Fragment {
             ViewGroup container, Bundle savedInstanceState) {
         toolsViewModel =
                 ViewModelProviders.of(this).get(ToolsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_tools, container, false);
+        View root = inflater.inflate(R.layout.fragment_message, container, false);
         final TextView textView = root.findViewById(R.id.text_tools);
         toolsViewModel.getText().observe(this, new Observer<String>() {
             @Override
