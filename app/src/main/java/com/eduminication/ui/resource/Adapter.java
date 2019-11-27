ce2ce65743e201aba8a44bb2d7acf05a1b669b6e
htliang25 <648029539@qq.com>
2019-11-27 22:50:43 +0800

    change

--- app/src/main/java/com/eduminication/ui/resource/Adapter.java
+++ app/src/main/java/com/eduminication/ui/resource/Adapter.java
@@ -1,59 +1,108 @@
 package com.eduminication.ui.resource;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import com.eduminication.R;
 import java.util.List;
 
-public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder>{
-    private List<String> mDatas;
+public class Adapter extends RecyclerView.Adapter implements View.OnClickListener {
+    private List<Item> dataList;
+    private Context context;
 
-    static class ViewHolder extends RecyclerView.ViewHolder{
-        View root;
-        TextView name;
+    public Adapter(Context context, List<Item> list) {
+        this.context = context;
+        dataList = list;
+    }
 
-        public ViewHolder(View view){
-            super(view);
-            root = view;
-            name = view.findViewById(R.id.name);
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.resource_item, parent, false);
+        return new ItemHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
+        Item item = dataList.get(position);
+        ((ItemHolder) holder).name.setText(item.getName());
+        ItemHolder itemHolder = (ItemHolder) holder;
+        itemHolder.itemView.setTag(position);
+        itemHolder.course1.setTag(position);
+        itemHolder.course2.setTag(position);
+        itemHolder.course3.setTag(position);
+        itemHolder.course4.setTag(position);
+        itemHolder.course5.setTag(position);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+
+    public class ItemHolder extends RecyclerView.ViewHolder {
+        public TextView name;
+        public TextView course1, course2, course3, course4, course5;
+
+        public ItemHolder(View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.name);
+            course1 = itemView.findViewById(R.id.course1);
+            course2 = itemView.findViewById(R.id.course2);
+            course3 = itemView.findViewById(R.id.course3);
+            course4 = itemView.findViewById(R.id.course4);
+            course5 = itemView.findViewById(R.id.course5);
+            //将创建的View注册点击事件
+            itemView.setOnClickListener(Adapter.this);
+            course1.setOnClickListener(Adapter.this);
+            course2.setOnClickListener(Adapter.this);
+            course3.setOnClickListener(Adapter.this);
+            course4.setOnClickListener(Adapter.this);
+            course5.setOnClickListener(Adapter.this);
         }
     }
 
-    @NonNull
-    @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.resource_item, parent, false);
-        final ViewHolder holder = new ViewHolder(view);
-        holder.root.setOnClickListener(new View.OnClickListener(){
-           @Override
-           public void onClick(View view){
-               int position = holder.getAdapterPosition();
-               String name = mDatas.get(position);
-               Toast.makeText(view.getContext(), "你点击了View" + name, Toast.LENGTH_SHORT).show();
-           }
-        });
-        return holder;
+
+    ////////////////////////////以下为item点击处理///////////////////////////////
+
+    private OnRecyclerViewItemClickListener mOnItemClickListener = null;
+
+    public void setOnItemClickListener(OnRecyclerViewItemClickListener listener) {
+        this.mOnItemClickListener = listener;
+    }
+
+    /** item里面有多个控件可以点击 */
+    public enum ViewName {
+        ITEM,
+        PRACTISE
+    }
+
+    public interface OnRecyclerViewItemClickListener {
+        void onClick(View view, ViewName viewName, int position);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position){
-        String mName = mDatas.get(position);
-        holder.name.setText(mName);
-    }
-
-    @Override
-    public int getItemCount(){
-        return mDatas.size();
-    }
-
-    public Adapter(List<String> mDatas){
-        this.mDatas = mDatas;
+    public void onClick(View v) {
+        //注意这里使用getTag方法获取数据
+        int position = (int) v.getTag();
+        if (mOnItemClickListener != null) {
+            switch (v.getId()){
+                case R.id.name:
+                    mOnItemClickListener.onClick(v, ViewName.PRACTISE, position);
+                    break;
+                default:
+                    mOnItemClickListener.onClick(v, ViewName.ITEM, position);
+                    break;
+            }
+        }
     }
 }
